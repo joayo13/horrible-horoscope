1bd308f20d4da0ca1928c81ef74ef60f30955e7c
Jordan Ayotte <jordanayotte13@outlook.com>
2023-07-11 13:45:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [sign, setSign] = useState(undefined)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center bg-red-700 h-screen text-white">
+    <h1 className="text-6xl">The Horrible Horoscope</h1>
+    <ul className="flex flex-wrap gap-8 justify-center px-1 py-8 md:text-lg underline">
+    <button>Aries</button>
+    <button>Taurus</button>
+    <button>Gemini</button>
+    <button>Cancer</button>
+    <button>Leo</button>
+    <button>Virgo</button>
+    <button>Libra</button>
+    <button>Scorpio</button>
+    <button>Sagittarius</button>
+    <button>Capricorn</button>
+    <button>Aquarius</button>
+    <button>Pisces</button>
+    </ul>
+    {sign ?
+    <div>
+    <h2 className="p-1 text-xl md:text-4xl">Daily Horrible Horoscope for {'Sagittarius'}</h2>
+    <p className="p-1 py-8 md:text-lg">Example horoscope font</p>
+    </div> 
+    : <h2 className="p-1 text-xl md:text-4xl">Select your sign from the list above.<br></br>...Or don't. Nobody really cares.</h2>}
+    <footer className="absolute bottom-0 p-1">Github link</footer>
     </div>
   );
 }
