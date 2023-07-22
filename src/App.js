86d331968c047e6c60521dfbd4319cfbef3ada89
Jordan Ayotte <jordanayotte13@outlook.com>
2023-07-22 12:50:50 -0400

    added firebase basic shit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import { useState } from "react";
 import './animations.css'
 import githubMark from './github-mark-white.png'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACnLM3Ha4CZZ8GGsFPh2iTOKQRZ-Kwyn0",
+  authDomain: "horrible-horoscope.firebaseapp.com",
+  projectId: "horrible-horoscope",
+  storageBucket: "horrible-horoscope.appspot.com",
+  messagingSenderId: "525137369496",
+  appId: "1:525137369496:web:c87998b71ce201101ef3ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 
 function App() {
   const [sign, setSign] = useState(undefined)
   const [newSignDetected, setNewSignDetected] = useState(false)
-  function handleSignSelect(e) {
+  const [horoscope, setHoroscope] = useState(undefined)
+  async function handleSignSelect(e) {
     const signName = e.target.id
+    let horoscopeArray = []
     console.log(signName)
+    const querySnapshot = await getDocs(collection(db, "horoscopes"));
+    querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    horoscopeArray.push(doc.data().horoscope);
+});
+    setHoroscope(horoscopeArray[Math.floor(Math.random() * horoscopeArray.length)])
     setSign(signName)
     setNewSignDetected(true)
     setTimeout(() => {
       setNewSignDetected(false)
-    }, 300)
+    }, 500)
   }
   return (
     <div className="text-center py-4 h-screen text-neutral-200">
@@ -31,12 +60,12 @@ function App() {
     <button id="Sagittarius" className="text-black z-30 rounded-full py-2 px-4 bg-purple-500 "onClick={(e) => handleSignSelect(e)}>Sagittarius</button>
     <button id="Capricorn"   className="text-black z-30 rounded-full py-2 px-4 bg-gray-500 "onClick={(e) => handleSignSelect(e)}>Capricorn</button>
     <button id="Aquarius"    className="text-black z-30 rounded-full py-2 px-4 bg-blue-500 "onClick={(e) => handleSignSelect(e)}>Aquarius</button>
-    <button id="Pisces"      className="text-black z-30 rounded-full py-2 px-4 bg-green-200"onClick={(e) => handleSignSelect(e)}>Pisces</button>
+    <button id="Pisces"      className="text-black z-30 rounded-full py-2 px-4 bg-green-300"onClick={(e) => handleSignSelect(e)}>Pisces</button>
     </ul>
     {sign ?
     <div className={newSignDetected ? "fade-in-slow relative z-30" : "relative z-30"}>
     <h2 className="p-1 text-xl md:text-4xl">Daily Horrible Horoscope for {sign}</h2>
-    <p className="p-1 py-8 md:text-lg">{sign} horoscope</p>
+    <p className="p-1 py-8 md:text-lg">Today {sign} {horoscope}</p>
     <p>Want to contribute to the project? <a className="underline" href='/'>Write some horrible horoscopes</a> of your own to be added to the mix.</p>
     </div> 
     : <h2 className="p-1 text-xl md:text-4xl relative z-30">Select your sign from the list above.</h2>}
